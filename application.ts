2725d39c9ecb46dea3860657ae5811418d35a052
Jackie-Tran <jtran2009a@gmail.com>
2020-10-29 16:35:59 -0400

    Switched to php graphql server

--- application.ts
+++
@@ -1,6 +0,0 @@
-import { createApplication } from 'graphql-modules';
-import { ticketModule } from './modules/ticket';
-
-export const application = createApplication({
-    modules: [ticketModule],
-});
